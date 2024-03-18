cdc3cabd1d9a3902e8a4ea8621c07c0a9266d268
Fabrikpz <fabrisilveyra@gmail.com>
2024-03-18 10:08:25 -0300

    tp3

---
+++ tp3/ej3/facil.py
@@ -0,0 +1,17 @@
+personas = {
+    "juan": 12,
+    "carlos": 30,
+    "marcos": 18,
+    "pedro": 26
+}
+
+#agregar:
+personas["pepe"] = 20
+
+#eliminar:
+personas.pop("juan")
+
+#buscar:
+edadcarlos = personas["carlos"]
+print("edad carlos: ", edadcarlos)
+print(personas)
